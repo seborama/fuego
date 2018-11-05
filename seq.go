22f50f52367cd9e0ec0d5db7d913d70cbdbdabc4
seborama <seb.d.albion@outlook.com>
2018-11-05 23:21:32 +0000

    Updated docs + added Seq tests

--- seq.go
+++ seq.go
@@ -2,24 +2,54 @@ package fuego
 
 import "github.com/raviqqe/hamt"
 
+// A Seq is a sequence
 type Seq struct {
 	seq []hamt.Entry
 }
 
+// NewSeq creates a new Seq
 func NewSeq() Seq {
 	return Seq{}
 }
 
+// Size returns the number of elements in the Seq
 func (s Seq) Size() int {
 	return len(s.seq)
 }
 
+// Append an element
 func (s Seq) Append(e hamt.Entry) Seq {
 	return Seq{
 		seq: append(s.seq, e),
 	}
 }
 
+func (s Seq) find(e hamt.Entry) int {
+	for p, v := range s.seq {
+		if v.Equal(e) {
+			return p
+		}
+	}
+	return -1
+}
+
+// Remove the first occurrence of the given element
+func (s Seq) Remove(e hamt.Entry) Seq {
+	i := s.find(e)
+	if i < 0 {
+		cpy := make([]hamt.Entry, len(s.seq))
+		copy(cpy, s.seq)
+		return Seq{seq: cpy}
+	}
+
+	return Seq{
+		seq: append(s.seq[:i], s.seq[i+1:]...),
+	}
+}
+
+// AsGo returns the values of the Seq in a Go slice.
+// The Go slice is mutable.
+// The Go slice and Seq are disconnected: changes to one won't affect the other.
 func (s Seq) AsGo() []hamt.Entry {
 	cpy := make([]hamt.Entry, len(s.seq))
 	copy(cpy, s.seq)
--- seq_test.go
+++ seq_test.go
@@ -3,6 +3,7 @@ package fuego
 import (
 	"testing"
 
+	"github.com/raviqqe/hamt"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -18,4 +19,38 @@ func TestSeq_AsGo_Immutability(t *testing.T) {
 
 	seq[0] = EntryInt(1000)
 	assert.NotEqual(t, unit.seq[0], seq[0])
+}
+
+func TestSeq_AsGo(t *testing.T) {
+	tests := []struct {
+		name string
+		seq  Seq
+		want []hamt.Entry
+	}{
+		{
+			name: "Should preserve the order of items - case 1",
+			seq: NewSeq().
+				Append(EntryInt(1)).
+				Append(EntryInt(2)).
+				Append(EntryInt(3)),
+			want: []hamt.Entry{EntryInt(1), EntryInt(2), EntryInt(3)},
+		},
+		{
+			name: "Should preserve the order of items - case 2",
+			seq: NewSeq().
+				Append(EntryInt(3)).
+				Append(EntryInt(1)).
+				Append(EntryInt(2)).
+				Remove(EntryInt(1)).
+				Append(EntryInt(7)),
+			want: []hamt.Entry{EntryInt(3), EntryInt(2), EntryInt(7)},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.seq
+			got := s.AsGo()
+			assert.EqualValues(t, tt.want, got)
+		})
+	}
 }
