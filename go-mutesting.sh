b76a627ffebbcc97a345c4ce39e653d923b1363e
seborama <seb.d.albion@outlook.com>
2018-11-19 21:26:34 +0000

    Updated go-mutesting.sh

--- go-mutesting.sh
+++ go-mutesting.sh
@@ -4,5 +4,7 @@ tput sc ; echo -n -e "Go getting go-mutesting..." ; tput rc
 go get -t -v github.com/zimmski/go-mutesting/...
 
 tput sc ; echo -n -e "Running go-mutesting...   " ; tput rc
-go-mutesting --test-recursive ${GOPATH}/src/github.com/seborama/fuego/... | grep -Ev "^PASS"
+go-mutesting --verbose --test-recursive ${GOPATH}/src/github.com/seborama/fuego/... 2>&1 | grep -Ev "^PASS |^Mutate |^Enable "
+
+(( $(find . -iname '*.tmp' | wc -l | awk '{ print $1 }') > 0 )) && echo -e "\nWARNING - go-mutesting has not restored some files:" && find . -iname '*.tmp'
 
