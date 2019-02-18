1780865f3d9313efff89cc0de62d616570e25a4a
seborama <undisclosed>
2019-02-18 22:14:26 +0000

    Added to Collector: GroupingBy, Mapping, ToEntryMap, ToEntrySlice

--- collector.go
+++ collector.go
@@ -2,7 +2,7 @@ package fuego
 
 // TODO: evolve towards a go-style decorator pattern?
 type Collector struct {
-	supplier    Getter
+	supplier    Getter     // TODO: rename to "Supplier" with a method of "supply"
 	accumulator BiFunction // TODO: this should be a BiConsumer but is it against pure functional design?
 	// combiner BiFunction // this is for joining paralle collectors
 	finisher Function
@@ -14,4 +14,62 @@ func NewCollector(supplier Getter, accumulator BiFunction, finisher Function) Co
 		accumulator: accumulator,
 		finisher:    finisher,
 	}
+}
+
+type CollectorMiddleware func(Function, Collector) Collector
+
+func GroupingBy(classifier Function, collector Collector) Collector {
+	supplier := collector.supplier
+
+	accumulator := func(supplier Entry, entry Entry) Entry {
+		return collector.accumulator(supplier, classifier(entry))
+	}
+
+	finisher := collector.finisher
+
+	return NewCollector(supplier, accumulator, finisher)
+}
+
+func Mapping(mapper Function, collector Collector) Collector {
+	supplier := collector.supplier
+
+	accumulator := func(supplier Entry, entry Entry) Entry {
+		return collector.accumulator(supplier, mapper(entry))
+	}
+
+	finisher := collector.finisher
+
+	return NewCollector(supplier, accumulator, finisher)
+}
+
+func ToEntryMap() Collector {
+	var supplier = func() Entry { // TODO: use chan Entry instead?
+		return EntryMap{}
+	}
+
+	accumulator := func(supplier, entry Entry) Entry {
+		return supplier.(EntryMap).Append(entry.(Tuple2))
+	}
+
+	finisher := func(e Entry) Entry {
+		return e
+	}
+
+	return NewCollector(supplier, accumulator, finisher)
+}
+
+func ToEntrySlice() Collector {
+	var supplier = func() Entry { // TODO: use chan Entry instead?
+		return EntrySlice{}
+	}
+
+	accumulator := func(supplier, entry Entry) Entry {
+		return supplier.(EntrySlice).Append(entry)
+	}
+
+	finisher := func(e Entry) Entry {
+		return e
+	}
+
+	return NewCollector(supplier, accumulator, finisher)
 }
