514289d36c6360b985a2b1dc4a0d2381cdb590af
seborama <undisclosed>
2019-02-23 23:05:47 +0000

    Added EntryByte

---
+++ entrybyte_test.go
@@ -0,0 +1,23 @@
+package fuego_test
+
+import (
+	"testing"
+
+	ƒ "github.com/seborama/fuego"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEntryByte(t *testing.T) {
+	t.Log(ƒ.EntryByte(42))
+}
+
+func TestEntryByteHash(t *testing.T) {
+	assert.Equal(t, uint32(0), ƒ.EntryByte(0).Hash())
+	assert.Equal(t, uint32(42), ƒ.EntryByte(42).Hash())
+}
+
+func TestEntryByteEqual(t *testing.T) {
+	assert.True(t, ƒ.EntryByte(0).Equal(ƒ.EntryByte(0)))
+	assert.True(t, ƒ.EntryByte(42).Equal(ƒ.EntryByte(42)))
+	assert.False(t, ƒ.EntryByte(2).Equal(EntryFake{}))
+}
