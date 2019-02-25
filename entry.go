1835e231b2534c5929f58f009b772fa85f7e5b07
Seb C <seborama@users.noreply.github.com>
2019-02-25 06:30:45 +0000

    Comment format updated

--- entry.go
+++ entry.go
@@ -8,8 +8,9 @@ type Entry interface {
 }
 
 // EntriesEqual checks the equality of 2 Entry objects.
-// Note: EntriesEqual(&entry1, &entry2) will not produce the desired
-//       outcome with this method.
+// Note:
+// EntriesEqual(&entry1, &entry2) will not produce the desired
+// outcome with this method.
 func EntriesEqual(e1, e2 Entry) bool {
 	return (e1 == nil && e2 == nil) || (e1 != nil && e1.Equal(e2))
 }
